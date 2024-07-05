19270198d25441525cbdc6987123f53fea656230
vivek gondaliya <vivekgondaliya4723@gmail.com>
2024-07-05 22:13:10 +0530

    initialize

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../features/navbar/Navbar'
+import ProductList from '../features/product-list/ProductList'
+
+const Home = () => {
+  return (
+    <Navbar >
+        <ProductList></ProductList>
+    </Navbar>
+  )
+}
+
+export default Home
